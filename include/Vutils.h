071075ce9eb04dcd372977f187851a9a0ed24a60
Vic P <vic4key@gmail.com>
2018-11-04 03:26:07 +0700

    Vutils

--- include/Vutils.h
+++ include/Vutils.h
@@ -51,7 +51,7 @@
 
 #define VU_STRINGIZE_EX(s) #s
 #define VU_STRINGIZE(s) VU_STRINGIZE_EX(s)
-#define VU_3RD_INCL(s)  VU_STRINGIZE(../third-parties/s)
+#define VU_3RD_INCL(s)  VU_STRINGIZE(../3rdparty/s)
 
 /* Header Inclusions */
 
