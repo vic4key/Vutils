10ec53ce2e1ebdc7ac50d61c6d760b99033b7016
Vic P <vic4key@gmail.com>
2020-05-23 00:34:26 +0700

    Vutils

--- Test/Sample.Math.h
+++ Test/Sample.Math.h
@@ -7,5 +7,46 @@ DEF_SAMPLE(Math)
   std::cout << "GCD : " << vu::GCD(3, 9, 27, 81) << std::endl;
   std::cout << "LCM : " << vu::LCM(3, 9, 27, 81) << std::endl;
 
+  {
+    vu::Point2DT<int> p1(1, 2), p2(3, 4), p3;
+    p3 =  p1;
+    p3 == p1;
+    p3 != p2;
+    p3 += p1;
+    p3 -= p2;
+    p3 -  p1;
+    p3 +  p2;
+    std::tcout << p1 << std::endl;
+    std::tcout << p2 << std::endl;
+    std::tcout << p3 << std::endl;
+  }
+
+  {
+    vu::Point3DT<int> p1(1, 2, 3), p2(4, 5, 6), p3;
+    p3 =  p1;
+    p3 == p1;
+    p3 != p2;
+    p3 += p1;
+    p3 -= p2;
+    p3 -  p1;
+    p3 +  p2;
+    std::tcout << p1 << std::endl;
+    std::tcout << p2 << std::endl;
+    std::tcout << p3 << std::endl;
+  }
+
+  {
+    vu::Vector2DT<int> v2(1, 2);
+    vu::Vector3DT<int> v3(1, 2, 3);
+    std::tcout << v2 << std::endl;
+    std::tcout << v3 << std::endl;
+  }
+
+  {
+    vu::RectT<int> rect(100, 200);
+    rect.Center();
+    std::tcout << rect << std::endl;
+  }
+
   return vu::VU_OK;
 }
