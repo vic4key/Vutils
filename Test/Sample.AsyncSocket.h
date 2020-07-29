7e9246ae44dab19b5c085ffa38b5331a7ff5157f
vic4key <vic4key@gmail.com>
2020-07-29 17:53:08 +0700

    Vutils

--- Test/Sample.AsyncSocket.h
+++ Test/Sample.AsyncSocket.h
@@ -2,55 +2,24 @@
 
 #include "Sample.h"
 
-class CAsyncSocketServer : public vu::CAsyncSocket
-{
-public:
-  CAsyncSocketServer() : vu::CAsyncSocket() {};
-  virtual ~CAsyncSocketServer() {};
-
-  virtual void OnOpen(vu::CSocket& client)
-  {
-    printf("\n");
-    printf("client %d opened\n", client.GetRemoteSAI().sin_port);
-  }
-
-  virtual void OnClose(vu::CSocket& client)
-  {
-    printf("client %d closed\n", client.GetRemoteSAI().sin_port);
-  }
-
-  virtual void OnSend(vu::CSocket& client)
-  {
-    std::string s = "hello from server";
-    client.Send(s.data(), int(s.size()));
-    printf("client %d send `%s`\n", client.GetRemoteSAI().sin_port, s.c_str());
-  }
-
-  virtual void OnRecv(vu::CSocket& client)
-  {
-    vu::CBuffer data(KiB);
-    client.Recv(data);
-    std::string s(reinterpret_cast<char*>(data.GetpBytes()));
-    printf("client %d recv `%s`\n", client.GetRemoteSAI().sin_port, s.c_str());
-  }
-};
+void fnExampleBinding(const vu::CSocket::TEndPoint& endpoint);
+void fnExampleInheritance(const vu::CSocket::TEndPoint& endpoint);
 
 DEF_SAMPLE(AsyncSocket)
 {
   #if defined(VU_SOCKET_ENABLED)
 
   const vu::CSocket::TEndPoint EndPoint("127.0.0.1", 1609);
+  fnExampleBinding(EndPoint);
+  // fnExampleInheritance(EndPoint);
 
-  /*// Example 1 : With derived class and their override methods
+  #endif // VU_SOCKET_ENABLED
 
-  CAsyncSocketServer server;
-  server.Bind(EndPoint);
-  server.Listen();
-  server.Run();
-  server.Close();*/
+  return vu::VU_OK;
+}
 
-  // Example 2 : With base class and mapping lambda functions
-
+void fnExampleBinding(const vu::CSocket::TEndPoint& endpoint)
+{
   vu::CAsyncSocket server;
 
   server.On(vu::CAsyncSocket::eFnType::OPEN, [](vu::CSocket& client) -> void
@@ -61,7 +30,7 @@ DEF_SAMPLE(AsyncSocket)
 
   server.On(vu::CAsyncSocket::eFnType::CLOSE, [](vu::CSocket& client) -> void
   {
-      printf("client %d closed\n", client.GetRemoteSAI().sin_port);
+    printf("client %d closed\n", client.GetRemoteSAI().sin_port);
   });
 
   server.On(vu::CAsyncSocket::eFnType::SEND, [](vu::CSocket& client) -> void
@@ -75,16 +44,54 @@ DEF_SAMPLE(AsyncSocket)
   {
     vu::CBuffer data(KiB);
     client.Recv(data);
-    std::string s(reinterpret_cast<char*>(data.GetpBytes()));
+    std::string s(reinterpret_cast<const char*>(data.GetpBytes()), data.GetSize());
     printf("client %d recv `%s`\n", client.GetRemoteSAI().sin_port, s.c_str());
   });
 
-  server.Bind(EndPoint);
+  server.Bind(endpoint);
   server.Listen();
   server.Run();
   server.Close();
+}
 
-  #endif // VU_SOCKET_ENABLED
+void fnExampleInheritance(const vu::CSocket::TEndPoint& endpoint)
+{
+  class CAsyncSocketServer : public vu::CAsyncSocket
+  {
+  public:
+    CAsyncSocketServer() : vu::CAsyncSocket() {};
+    virtual ~CAsyncSocketServer() {};
 
-  return vu::VU_OK;
+    virtual void OnOpen(vu::CSocket& client)
+    {
+      printf("\n");
+      printf("client %d opened\n", client.GetRemoteSAI().sin_port);
+    }
+
+    virtual void OnClose(vu::CSocket& client)
+    {
+      printf("client %d closed\n", client.GetRemoteSAI().sin_port);
+    }
+
+    virtual void OnSend(vu::CSocket& client)
+    {
+      std::string s = "hello from server";
+      client.Send(s.data(), int(s.size()));
+      printf("client %d send `%s`\n", client.GetRemoteSAI().sin_port, s.c_str());
+    }
+
+    virtual void OnRecv(vu::CSocket& client)
+    {
+      vu::CBuffer data(KiB);
+      client.Recv(data);
+      std::string s(reinterpret_cast<char*>(data.GetpBytes()));
+      printf("client %d recv `%s`\n", client.GetRemoteSAI().sin_port, s.c_str());
+    }
+  };
+
+  CAsyncSocketServer server;
+  server.Bind(endpoint);
+  server.Listen();
+  server.Run();
+  server.Close();
 }
