ebd4fb2698146926b76f6b1c5c7578b75268d5b8
Vic P <vic4key@gmail.com>
2021-09-21 03:47:40 +0700

    Vutils

--- Test/Test.WH.DLL/WH.cpp
+++ Test/Test.WH.DLL/WH.cpp
@@ -5,18 +5,18 @@
 
 LRESULT CALLBACK fnCBTProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-  vu::MsgA("[CBT] nCode = %d, wParam = %08X, lParam = %08X", nCode, wParam, lParam);
+  vu::msg_debug_A("[CBT] nCode = %d, wParam = %08X, lParam = %08X", nCode, wParam, lParam);
   return CallNextHookEx(nullptr, nCode, wParam, lParam);
 }
 
 LRESULT CALLBACK fnMouseProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-  vu::MsgA("[Mouse] nCode = %d, wParam = %08X, lParam = %08X", nCode, wParam, lParam);
+  vu::msg_debug_A("[Mouse] nCode = %d, wParam = %08X, lParam = %08X", nCode, wParam, lParam);
   return CallNextHookEx(nullptr, nCode, wParam, lParam);
 }
 
 LRESULT CALLBACK fnKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-  vu::MsgA("[Keyboard] nCode = %d, wParam = %08X, lParam = %08X", nCode, wParam, lParam);
+  vu::msg_debug_A("[Keyboard] nCode = %d, wParam = %08X, lParam = %08X", nCode, wParam, lParam);
   return CallNextHookEx(nullptr, nCode, wParam, lParam);
 }
