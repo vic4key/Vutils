02c25291bdeef10a2c94527ac3379f07ab5e135b
Vic P <vic4key@gmail.com>
2022-09-20 13:43:29 +0700

    Vutils

--- Test/Sample.Misc.h
+++ Test/Sample.Misc.h
@@ -208,12 +208,12 @@ DEF_SAMPLE(Misc)
 
   #if ((defined(_MSVC_LANG) && _MSVC_LANG >= 201402L) || (__cplusplus >= 201402L))
   {
-    #define container vector
-    container<int>    l_int = { 1, 2, 3, 4, 5 
-    container<char>   l_chr = { '1', '2', '3', '4', '5' };};
-    container<float>  l_flt = { 1.F, 2.F, 3.F, 4.F, 5.F };
+    #define container std::vector // std::list // std::set // etc
+    container<int> l_int = { 1, 2, 3, 4, 5 
+    container<char> l_chr = { '1', '2', '3', '4', '5' };};
+    container<float> l_flt = { 1.F, 2.F, 3.F, 4.F, 5.F };
     container<double> l_dbl = { 1.0, 2.0, 3.0, 4.0, 5.0 };
-    container<string> l_str = { "1", "2", "3", "4", "5" };
+    container<std::string> l_str = { "1", "2", "3", "4", "5" };
     using namespace std::op;
     std::tcout << (1   <in> l_int) << std::endl;
     std::tcout << ('1' <in> l_chr) << std::endl;
